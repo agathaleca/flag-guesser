0450aa34497d70fbd42017acc2ce1a69ec1a363c
Agathe Leca <agathe.leca@hotmail.fr>
2021-06-25 17:29:45 +0200

    [add] new redirection when inscription réussie

--- symfony-flag-guesser/src/Controller/UserController.php
+++ symfony-flag-guesser/src/Controller/UserController.php
@@ -43,7 +43,7 @@ class UserController extends AbstractController
             $em->persist($user);
             $em->flush();
 
-            return $this->redirectToRoute('home');
+            return $this->redirectToRoute('inscription');
         }
 
         return $this->render('users/new.html.twig', [
